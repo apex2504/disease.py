3228e33eb20501cf7587050ca33d3651a96c5d87
Rob Wainwright <47543882+apex2504@users.noreply.github.com>
2020-03-19 22:32:17 +0000

    add StateStatistics to init

--- corona_api/__init__.py
+++ corona_api/__init__.py
@@ -1,3 +1,3 @@
 from .client import Client
-from .statistics import GlobalStatistics, CountryStatistics
+from .statistics import GlobalStatistics, CountryStatistics, StateStatistics
 from .utils import format_date, format_number
