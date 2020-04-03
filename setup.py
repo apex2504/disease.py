1c67ceca857ceec4422301b73342788819498c41
Rob Wainwright <47543882+apex2504@users.noreply.github.com>
2020-04-03 20:59:03 +0100

    update to stable

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
           'aiohttp',
       ],
   classifiers=[
-    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
